bc354aa1147a8417bc31ea74b7507dc2329443c3
York Wu <smallwater94@gmail.com>
2021-07-13 23:27:25 +0800

    Update some reply

--- app.py
+++ app.py
@@ -37,9 +37,16 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    r = '講點別的 好嗎'
+
+
+    if msg == '新北王先生':
+        r = '一路大順暢!'
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
