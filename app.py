ed2f30e342beedf75213bc22a5cafee02052079c
York Wu <smallwater94@gmail.com>
2021-07-13 23:42:16 +0800

    Add a new reply

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+import time
+
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('iCw04qORsvX81qjY77WP2KH9TBrk0/WQX2RIgPm2wIYDU7URknX2doJLkl9X15bi7acNUqWyjNkr5Gd0HZhYfBrQnhAX6e59E44pfytqZL/CBuCEzC9BMZc97rHJYOUdqjp41DcgBIaPY9b/7uSlUwdB04t89/1O/w1cDnyilFU=')
@@ -43,11 +45,22 @@ def handle_message(event):
 
     if msg == '新北王先生':
         r = '一路大順暢!'
+    elif mas in ['內湖','塞車','內湖有塞車嗎','內湖塞車嗎','內湖塞車']:
+        r = '很誇張'
+        replymsg(r)
+        time.sleep(0.3)
+        r = '我有行車紀錄器你知道嘛'
+        replymsg(r)
+        time.sleep(0.8)
+        r = '很誇張'
+    else:
+        replymsg(r)
 
+
+def replymsg(r):
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=r))
-
 
 if __name__ == "__main__":
     app.run()
