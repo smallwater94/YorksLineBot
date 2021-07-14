67068129e8e514e9d4f6cdb513a1a3500b5c54a0
York Wu <smallwater94@gmail.com>
2021-07-14 16:37:37 +0800

    Add user's manual

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def handle_message(event):
             sticker_message)
         return
 
-    r = '特殊指令有:["新北王先生"，"塞車"] 隨機貼圖請輸入:"R"'
+    r = '特殊指令有:\n["新北王先生"]\n["塞車"]\n隨機貼圖請輸入:\n["R"]'
     if msg == '新北王先生':
         r = '一路大順暢!'
     elif '塞車' in msg:
